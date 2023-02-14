7d8b2391279a8a514207585610b447057e7ea6d2
calculatortamer <calculatortamer@protonmail.com>
2023-02-14 17:37:28 +0100

    add FullCanvas implementation for nokia games

---
+++ java/midp/com/nokia/mid/ui/FullCanvas.java
@@ -0,0 +1,11 @@
+// thanks to http://www.j2megame.org/j2meapi/Nokia_UI_API_1_1/
+
+package com.nokia.mid.ui;
+
+public abstract class FullCanvas
+extends javax.microedition.lcdui.Canvas {
+
+protected FullCanvas(){
+super.setFullScreenMode(true);
+}
+}
